1a948c790e905cef295e1f44fdf954c2a2fe29d5
MrYuriy <yuriybabiyk@gmail.com>
2024-02-04 00:12:52 +0100

    Add cors

--- cups/settings.py
+++ cups/settings.py
@@ -139,3 +139,7 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CORS_ALLOWED_ORIGINS = [
+    "*",
+]

fd59c1340bf3d062f70928764e4684f3e5508dc7
MrYuriy <yuriybabiyk@gmail.com>
2024-02-09 08:50:21 +0100

    Add connection to PostgreSQL

--- cups/settings.py
+++ cups/settings.py
@@ -84,12 +84,17 @@ WSGI_APPLICATION = "cups.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+if DATABASE_URL is not None:
+    DATABASES = {
+        "default": dj_database_url.config(
+            default=DATABASE_URL,
+            conn_max_age=600,
+            conn_health_checks=True,
+        ),
     }
-}
+
 
 
 # Password validation

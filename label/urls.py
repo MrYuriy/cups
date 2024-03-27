1d87f57c37bc41fe65c3cc17b3cd287db993e8a2
MrYuriy <yuriybabiyk@gmail.com>
2024-03-27 15:58:59 +0100

    Add reprint endpoint

--- label/urls.py
+++ label/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import LabelListView, my_view
+from .views import LabelListView, reprint_label ,my_view
 
 urlpatterns = [
     path("labels/", LabelListView.as_view(), name="labels"),
+    path("labels/reprint/", reprint_label, name="label_reprint"),
     path("my-url/", my_view, name='my-url-name'),
 ]
 app_name = "label"
--- label/views.py
+++ label/views.py
@@ -6,6 +6,7 @@ from .models import Label
 from .serializers import LabelSerializer
 from .utils import generate_label
 from django.http import JsonResponse
+from rest_framework.decorators import api_view
 
 # Create your view
 
@@ -25,6 +26,24 @@ class LabelListView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST'])
+def reprint_label(request):
+    identifier = request.data.get("identifier")
+    exsist_label = Label.objects.filter(identifier=identifier).first()
+    if exsist_label:
+        exsist_label.print_status = False
+        exsist_label.save()
+        return Response(status=status.HTTP_201_CREATED)
+
+    serializer = LabelSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.print_status = False
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 def my_view(request):
     # Your view logic here

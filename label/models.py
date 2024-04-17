88e70b43c51c50e8ab7d9314d4a83a8fec6a644e
MrYuriy <yuriybabiyk@gmail.com>
2024-04-17 16:27:57 +0200

    Fix ampty field in label

--- label/models.py
+++ label/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 class Label(models.Model):
     supplier_company = models.CharField(max_length=100)
-    shop = models.CharField(max_length=3)
-    user = models.CharField(max_length=50)
-    data = models.CharField(max_length=10)
-    order = models.CharField(max_length=8)
-    identifier = models.CharField(max_length=12)
-    comment = models.CharField(max_length=100)
+    shop = models.CharField(max_length=3, default="", null=True, blank=True)
+    user = models.CharField(max_length=50, default="", null=True, blank=True)
+    data = models.CharField(max_length=10, default="", null=True, blank=True)
+    order = models.CharField(max_length=8, default="", null=True, blank=True)
+    identifier = models.CharField(max_length=12, default="", null=True, blank=True)
+    comment = models.CharField(max_length=100, default="", null=True, blank=True)
     print_status = models.BooleanField(default=False)
 
     def __str__(self):

4267d2e3f5f82984f042e8f774c2c408be615d98
MrYuriy <yuriybabiyk@gmail.com>
2024-10-02 08:00:04 +0200

    Add exsta values for stock label

--- label/utils.py
+++ label/utils.py
@@ -22,8 +22,11 @@ def generate_label_stock(label_ins):
             f"^XA^FO 30,70^FB500,2,10,L,0^ATN,30,30^FD Dostawca:  {label_inst.supplier_company}^FS"
             f"^FO 18,240 ^AUN,50,50^FD USER: {label_inst.user}^FS"
             f"^FO 18,320 ^AUN,50,50^FD DATA: {label_inst.data}^FS"
-            f"^FO 18,400 ^AUN,50,50^FD Order: {label_inst.pre_advice} ^FS"
+            f"^FO 18,400 ^AUN,50,50^FD Master :: {label_inst.master_id} ^FS"
             f"^FO 30,500^FB500,3,10,L,0^CI28^ATN,100,100^FD{label_inst.comment}^FS"
+            f"^FO 18,600 ^AUN,50,50^FD Order: {label_inst.pre_advice}^FS"
+            f"^FO 18,700 ^AUN,50,50^FD SKU:  {label_inst.sku}^FS"
+            f"^FO 30,800^FB700,2,10,L,0^ATN,30,30^FD Deskription: {label_inst.deskription}^FS"
             f"^FO 580,90^BY3^BCB,120,N,N,N^FD{label_inst.identifier}^FS"
             f"^FO700,120 ^ASB,90,70 ^FD {label_inst.identifier}^XZ"
         )

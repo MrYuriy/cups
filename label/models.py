67a591dfb912317de5865318a128bb485125f452
MrYuriy <yuriybabiyk@gmail.com>
2024-10-25 13:11:32 +0200

    Extend pre_advice lenght

--- label/models.py
+++ label/models.py
@@ -18,7 +18,7 @@ class LabelStock(models.Model):
     supplier_company = models.CharField(max_length=100)
     user = models.CharField(max_length=50, default="", null=True, blank=True)
     data = models.CharField(max_length=10, default="", null=True, blank=True)
-    pre_advice = models.CharField(max_length=8, default="", null=True, blank=True)
+    pre_advice = models.CharField(max_length=12, default="", null=True, blank=True)
     master_id = models.CharField(max_length=15, default="", null=True, blank=True)
     identifier = models.CharField(max_length=12, default="", null=True, blank=True)
     delivery_part = models.CharField(max_length=12, default="", null=True, blank=True)

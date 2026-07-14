78fa028d321c1615652a9c216fe67ac282a51327
Yurii Babiuk <90417593+MrYuriy@users.noreply.github.com>
2026-07-14 22:37:32 +0200

    Fix CORS: use CORS_ALLOW_ALL_ORIGINS (corsheaders system check)

--- cups/settings.py
+++ cups/settings.py
@@ -172,8 +172,6 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-CORS_ALLOWED_ORIGINS = [
-    "*",
-]
+CORS_ALLOW_ALL_ORIGINS = True
 SECURE_SSL_REDIRECT = False
 SESSION_COOKIE_SECURE = False
